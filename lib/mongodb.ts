4d8128df6c494dbb0218cd03b8040287104085c8
solace <monkdev9@gmail.com>
2026-04-18 23:47:11 +0100

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,29 +1,19 @@
-import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
 
-// Extend the NodeJS global type to store a cached connection.
+const uri = process.env.MONGO_URI;
+
+if (!uri) {
+  throw new Error("Missing MONGO_URI");
+}
+
 declare global {
   // eslint-disable-next-line no-var
-  var mongooseConnection: any;
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-/**
- * Connect to MongoDB using Mongoose. Uses a global cached connection to
- * prevent creating multiple connections in development or when hot reloading.
- */
-export async function connectDB() {
-  if (global.mongooseConnection && global.mongooseConnection.conn) {
-    return global.mongooseConnection.conn;
-  }
-  if (!global.mongooseConnection) {
-    global.mongooseConnection = { conn: null, promise: null };
-  }
-  if (!global.mongooseConnection.promise) {
-    const uri = process.env.MONGO_URI;
-    if (!uri) {
-      throw new Error("MONGO_URI is not defined in environment variables");
-    }
-    global.mongooseConnection.promise = mongoose.connect(uri).then((mongoose) => mongoose);
-  }
-  global.mongooseConnection.conn = await global.mongooseConnection.promise;
-  return global.mongooseConnection.conn;
-}
+const client = new MongoClient(uri);
+
+const clientPromise =
+  global._mongoClientPromise ?? (global._mongoClientPromise = client.connect());
+
+export default clientPromise;
